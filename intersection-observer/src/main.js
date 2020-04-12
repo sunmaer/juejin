f648f149fa7d6aa53a220fae6385453e93a36e96
menghaisun <menghaisun@tencent.com>
2020-04-12 14:03:07 +0800

    feat: 增加polyfill

--- intersection-observer/src/main.js
+++ intersection-observer/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import axios from 'axios';
 import App from './App.vue';
 import './modules/mock';
+import 'intersection-observer'
 
 Vue.config.productionTip = false;
 Vue.prototype.$ajax = axios;
